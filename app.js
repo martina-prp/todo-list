25f9c8e070f59abffa00f0eaa412d0c993bdaed6
martina-prp <radeva_14@abv.bg>
2014-08-18 22:07:12 +0300

    forst initial commit on the task.

---
+++ app.js
@@ -0,0 +1,45 @@
+'use strict';
+
+$(document).ready(function() {
+
+  $('.create-task').on('click', function(event) {
+    event.preventDefault();
+    var taskTitle = $('.task-title').val();
+    var taskDate = $('.task-date').val();
+    var taskDescription = $('.task-description').val();
+    var taskStarred = $('input[name="task-starred"').is(':checked');
+
+    var newTask = new Task(taskTitle, taskDate, taskDescription, taskStarred);
+    var tempData = localStorage.getItem('task-manager'),
+          data = [];
+      if (tempData) {
+        //console.log(tempData);
+        data = JSON.parse(tempData);
+      }
+      data.push(newTask);
+      localStorage.setItem('task-manager', JSON.stringify(data));
+  });
+
+  function Task(title, date, description, star) {
+    this.title = title;
+    this.date = date;
+    this.description = description;
+    this.star = star;
+
+    this.getTitle = function() {
+      return this.title;
+    }
+
+    this.getDate = function() {
+      return this.date;
+    }
+
+    this.getDescription = function() {
+      return this.description;
+    }
+
+    this.getStar = function() {
+      return this.star;
+    }
+  }
+});
