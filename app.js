a685854f7c599de3f0cc7aa22cbb884f6d37c1a1
Martina Radeva <martina@propeople.dk>
2014-08-19 18:15:15 +0300

    add task table.

--- app.js
+++ app.js
@@ -2,18 +2,27 @@
 
 $(document).ready(function() {
 
+  var tempData = localStorage.getItem('task-manager'),
+    data = [];
+  if (tempData) {
+    data = JSON.parse(tempData);
+    var taskTemp = $('#task-tpl').html();
+    var template = Handlebars.compile(taskTemp);
+    var html = template({tasks: data});
+    $('.task-table').append(html);
+  }
+
   $('.create-task').on('click', function(event) {
     event.preventDefault();
     var taskTitle = $('.task-title').val();
     var taskDate = $('.task-date').val();
     var taskDescription = $('.task-description').val();
-    var taskStarred = $('input[name="task-starred"').is(':checked');
+    var taskStarred = $('input[name="task-starred"]').is(':checked');
 
     var newTask = new Task(taskTitle, taskDate, taskDescription, taskStarred);
-    var tempData = localStorage.getItem('task-manager'),
-          data = [];
+    var tempData = localStorage.getItem('task-manager');
       if (tempData) {
-        //console.log(tempData);
+        console.log(tempData);
         data = JSON.parse(tempData);
       }
       data.push(newTask);
